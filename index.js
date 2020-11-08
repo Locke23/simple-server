9f68e9aef7d56088b30db5fc635fe91fe6165369
Lucas <lucascarmo6@gmail.com>
2020-11-08 18:05:39 -0300

    added aws sdk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const aws = require("aws-sdk");
 const express = require("express");
 
 const app = express();
@@ -6,7 +6,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ message: "server is up" });
+  var meta = new aws.MetadataService();
+
+  meta.request("/latest/meta-data/instance-id", function (err, data) {
+    console.log(data);
+  });
+  return res.send(`<html><body>${`servidor ta up`}</body></html>`);
 });
 
 app.post("/test", (req, res) => {
