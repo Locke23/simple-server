2960ee89b1f75d6c0e90bd5fcc4a39c5bfccaa0b
Lucas <lucascarmo6@gmail.com>
2020-11-08 18:20:22 -0300

    instance id method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const aws = require("aws-sdk");
+const AWS = require("aws-sdk");
 const express = require("express");
 
 const app = express();
@@ -6,15 +6,17 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  let meta = new aws.MetadataService();
-  let instanceId;
-  meta.request("/latest/meta-data/instance-id", function (err, data) {
-    instanceId = data;
-  });
-  return res.json({
-    status: "ok",
-    instanceId: instanceId || null,
-  });
+  let meta = new AWS.MetadataService();
+  try {
+    meta.request("/latest/meta-data/instance-id", function (err, data) {
+      return res.json({
+        status: "ok",
+        instanceId: data,
+      });
+    });
+  } catch (err) {
+    return res.json({ status: "ok", instanceId: "Nao foi possivel" });
+  }
 });
 
 app.post("/test", (req, res) => {
